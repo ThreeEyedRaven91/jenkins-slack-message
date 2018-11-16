2f8676724bfa0354bf1165df0d65a09121a76e4f
EugeneNguyen <xuanbinh91@gmail.com>
2018-11-16 11:03:43 +0700

    Finish sending with params

--- vars/terSlack.groovy
+++ vars/terSlack.groovy
@@ -1,13 +1,12 @@
 def message(args) {
-    def post = new URL("https://hooks.slack.com/services/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").openConnection();
-    def message = 'payload={"channel": "#jenkins-inno-insight", "username": "Jenkins", "text": "*InnoInsight* Build Started" }';
+    def post = new URL("https://hooks.slack.com/services/${args.hookKey}").openConnection();
+    def message = "payload={\"channel\": \"${args.channel}\", \"username\": \"Jenkins\", \"text\": \"${args.message}\" }";
     post.setRequestMethod("POST")
     post.setDoOutput(true)
     post.setRequestProperty("Content-Type", "application/x-www-form-urlencoded")
     post.getOutputStream().write(message.getBytes("UTF-8"));
     def postRC = post.getResponseCode();
-    println(postRC);
     if(postRC.equals(200)) {
-        println(post.getInputStream().getText());
+        println("[Slack] ${args.channel}: ${args.message}");
     }
 }
