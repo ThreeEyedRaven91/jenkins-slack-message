20abc8903b0aeea33c67d248c574a3b1877a4787
EugeneNguyen <xuanbinh91@gmail.com>
2018-11-16 11:30:45 +0700

    Add env

--- vars/terSlack.groovy
+++ vars/terSlack.groovy
@@ -1,12 +1,17 @@
 def message(args) {
-    def post = new URL("https://hooks.slack.com/services/${args.hookKey}").openConnection();
-    def message = "payload={\"channel\": \"${args.channel}\", \"username\": \"Jenkins\", \"text\": \"${args.message}\" }";
+    def hookKey = args.hookKey ? args.hookKey : env.TER_SLACK_HOOK_KEY
+    def channel = args.channel ? args.channel : env.TER_SLACK_CHANNEL
+
+    def post = new URL("https://hooks.slack.com/services/${hookKey}").openConnection()
+    def message = "payload={\"channel\": \"${channel}\", \"username\": \"Jenkins\", \"text\": \"${args.message}\" }"
+
     post.setRequestMethod("POST")
     post.setDoOutput(true)
     post.setRequestProperty("Content-Type", "application/x-www-form-urlencoded")
-    post.getOutputStream().write(message.getBytes("UTF-8"));
-    def postRC = post.getResponseCode();
+    post.getOutputStream().write(message.getBytes("UTF-8"))
+    def postRC = post.getResponseCode()
+
     if(postRC.equals(200)) {
-        println("[Slack] ${args.channel}: ${args.message}");
+        println("[Slack] ${channel}: ${args.message}")
     }
 }
